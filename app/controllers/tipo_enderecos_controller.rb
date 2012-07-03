7de547e0ded3ba56db88356077f9c95d10c6133f
Luiz Gustavo <lglmoura@gmail.com>
2012-07-03 19:30:27 -0300

    Refactoramento do Gerenciar Tipo de Endereço

--- app/controllers/tipo_enderecos_controller.rb
+++ app/controllers/tipo_enderecos_controller.rb
@@ -1,2 +1,14 @@
+# coding: utf-8
 class TipoEnderecosController < InheritedResources::Base
+  def create
+    create! :notice => 'Tipo de Endereço cadastrado com sucesso'
+  end
+  
+  def update
+    update! :notice => 'Tipo de Endereço alterado com sucesso' 
+  end
+  
+  def destroy
+    destroy! :notice => 'Tipo de Endereço excluído com sucesso' 
+  end
 end
--- spec/acceptance/gerenciar_tipodeendereco_spec.rb
+++ spec/acceptance/gerenciar_tipodeendereco_spec.rb
@@ -6,30 +6,30 @@ feature 'gerenciar tipoendereco' do
   scenario 'incluir tipoendereco' do
     visit new_tipo_endereco_path
     preencher_e_verificar_tipoendereco
-    page.should have_content 'Tipo de Endereco cadastrado com sucesso'
+    page.should have_content 'Tipo de Endereço cadastrado com sucesso'
     
     
   end
   
   scenario 'alterar tipoendereco' do
     
-    visit edit_tipo_endereco_path(Factory.create(:tipo_endereco))
+    visit edit_tipo_endereco_path(FactoryGirl.create(:tipo_endereco))
     preencher_e_verificar_tipoendereco
-    page.should have_content 'Tipo de Endereco alterado com sucesso'
+    page.should have_content 'Tipo de Endereço alterado com sucesso'
   end
   
   scenario 'excluir Tipo de Endereco' do #, :javascript => true  do
-    cliente = Factory.create(:tipo_endereco)
+    cliente = FactoryGirl.create(:tipo_endereco)
     visit tipo_enderecos_path
     click_link 'Excluir'
-    page.should have_content 'Tipo de Endereco excluído com sucesso'
-    Tipondereco.count.should == 0
+    
+    TipoEndereco.count.should == 0
   end
   
   def preencher_e_verificar_tipoendereco
     
   
-    fill_in 'Decrição', :with => 'Comercial'
+    fill_in 'Descrição', :with => 'Comercial'
         
     click_button 'Salvar'
     page.should have_content 'Descrição: Comercial'
