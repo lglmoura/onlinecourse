645c43e8d024c67582efb6b8a3936483bd090660
Luiz Gustavo <lglmoura@gmail.com>
2012-07-03 17:11:42 -0300

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlinecourse::Application.config.secret_token = 'c50e1e71932dc9057e9fc00715b8e2bb2e0b406027c9d8a58c8de20cf89415f2566cbd42ed1ce05251c7373147c70b18da69feb53007a3997285db1c04014281'
